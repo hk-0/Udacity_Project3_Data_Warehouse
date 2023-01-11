c4e1baae7f6d39ef9662341feb6ddf45792bbc1d
Hiren Khatri <63735062+hk-0@users.noreply.github.com>
2023-01-11 09:54:51 -0500

    Added docstrings to etl.py and create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,34 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drops each table using the queries in `drop_table_queries` list.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Creates each table using the queries in `create_table_queries` list. 
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    - Establishes connection with the redshift cluster and gets
+    cursor to it.  
+    
+    - Drops all the tables.  
+    
+    - Creates all tables needed. 
+    
+    - Finally, closes the connection. 
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
--- etl.py
+++ etl.py
@@ -1,21 +1,55 @@
+"""
+Script that connects to the redshift cluster, 
+loads data from S3 into staging tables 
+and then inserts into the facts and dimensionn tables
+"""
 import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Takes data from the S3 buckets and loads into the staging tables on Redshift using the COPY command
+    
+    Parameters
+    ----------
+    cur : object
+        Database connection cursor object
+    
+    conn : object
+        Database connection object
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Takes data from the staging tables and inserts it into the facts and dimension tables on Redshift
+    
+    Parameters
+    ----------
+    cur : object
+        Database connection cursor object
+    
+    conn : object
+        Database connection object
+    
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Reads the config file to get the redshift cluster parameters and connect to redshift.
+    Load data into the staging tables
+    Load data into the facts and dimension tables
+    Close connection to redshift
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
